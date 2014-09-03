3dfc621434c845b4ac04c3c401f419ae6a1fe391
Guilherme (Coder) de Souza <gui_souza@me.com>
2014-09-03 17:18:13 -0300

    nodeJS compatibility

--- dist/ordine.js
+++ dist/ordine.js
@@ -131,7 +131,6 @@
 ;(function(Ordine) {
 
   Ordine.prototype.run = function(){
-  	console.log('run');
   	this.loop();
     return this;
   };
--- src/methods/Ordine.run.js
+++ src/methods/Ordine.run.js
@@ -1,7 +1,6 @@
 ;(function(Ordine) {
 
   Ordine.prototype.run = function(){
-  	console.log('run');
   	this.loop();
     return this;
   };
